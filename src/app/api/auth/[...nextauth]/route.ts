9432fc48aa98221835d74e2be8cffe69f8f84167
Khoi T <truong.hnkhoi@outlook.com>
2024-01-09 16:10:01 +0700

    Adding Google Auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,18 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
+// import GoogleProvider from "next-auth/providers/google";
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
+
+// const handler = NextAuth({
+//     providers:[
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+//         }),
+//     ],
+// });
+
 export { handler as GET, handler as POST };
