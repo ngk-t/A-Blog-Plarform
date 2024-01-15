4327ff609b340223c08f98be89a48c7ab4050bdd
Khoi T <truong.hnkhoi@outlook.com>
2024-01-15 11:47:47 +0700

    Hopefully fixes Vercel error

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import { FaBars, FaMagnifyingGlass, FaGithub } from "react-icons/fa6";
 import { CgDarkMode } from "react-icons/cg";
-import { getServerAuthSession } from "~/server/auth";
+// import { getServerAuthSession } from "~/server/auth";
+// import { useSession } from "next-auth/react"
 
-const session = await getServerAuthSession();
+import { getServerAuthSession } from "../../server/auth";
+import { type GetServerSideProps } from "next";
 
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const session = await getServerAuthSession(ctx);
 
 export default function Navbar() {
+    // const session = await getServerAuthSession();
+    // const { data: session, status, update } = useSession()
     return(
         <nav className="h-25 fixed top-0 z-10 box-border w-full bg-white bg-opacity-25 p-4 backdrop-blur-md border-3 border-white border-opacity-10 shadow-lg shadow-neutral-200/80">
             <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
@@ -17,8 +23,8 @@ export default function Navbar() {
                     <Link href="/" className="text-black/70 no-underline hover:text-black">Home</Link>
                 </div>
                 <div className="prose prose-xl mx-auto flex flex-row allign-middle gap-10">
-                    
-                    <p> {session && <span>Create New Post</span>} </p>
+                    {/* {status === "authenticated" && (<p> <span>Create New Post</span> </p>)} */}
+                    {/* <p> {session && <span>Create New Post</span>} </p> */}
                     <Link href="https://github.com/ngk-t/A-Blog-Plarform" ><FaGithub /></Link>
                     <Link href="/" ><CgDarkMode /></Link>
                     <Link href="/" className="text-black/70 no-underline hover:text-black"><FaMagnifyingGlass /></Link>
